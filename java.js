f0264d55f347311310346cd16486e05eb2e41f1c
11chr <agabrielchr@gmail.com>
2023-02-22 23:10:20 +0200

    cookies button

--- java.js
+++ java.js
@@ -19,3 +19,40 @@ showMenu('header-toggle', 'navbar')
 // expandImage.addEventListener('click', () => {
 //     expandImage.classList.toggle('expand-image-fullscreen');
 // });
+
+var cookiePopup = document.getElementById('cookie-popup');
+var cookieAccept = document.getElementById('cookie-accept');
+
+if (getCookie('cookieAccepted') === '') {
+  cookiePopup.style.display = 'block';
+}
+
+cookieAccept.addEventListener('click', function() {
+  setCookie('cookieAccepted', 'true', 365);
+  cookiePopup.style.display = 'none';
+});
+
+function setCookie(name, value, days) {
+  var expires = '';
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + value + expires + '; path=/';
+}
+
+function getCookie(name) {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1, c.length);
+    }
+    if (c.indexOf(nameEQ) === 0) {
+      return c.substring(nameEQ.length, c.length);
+    }
+  }
+  return '';
+}
