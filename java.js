9dd771f0cc7cd5ec9e00919d4f13ff8ba0fa47fc
11chr <agabrielchr@gmail.com>
2023-11-25 00:49:50 +0200

    red

--- java.js
+++ java.js
@@ -1,3 +1,5 @@
+console.log("Script is running.");
+
 const showMenu = (headerToggle, navbarId) => {
   const toggleBtn = document.getElementById(headerToggle)
   const nav = document.getElementById(navbarId)
@@ -53,41 +55,8 @@ function getCookie(name) {
   return '';
 }
 
-// Gallery -------------------------------------------------------
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
 
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-function removeClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
+// Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
@@ -106,8 +75,8 @@ function validateForm() {
   var message = document.getElementById('message').value;
 
   if (name === '' || email === '' || message === '') {
-      alert('Please fill in all fields');
-      return false;
+    alert('Please fill in all fields');
+    return false;
   }
   return true;
 }
